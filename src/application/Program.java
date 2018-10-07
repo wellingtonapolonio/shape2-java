16ca56f1595808bbf56f38425d6b5c8deeb0898d
wellington <wellingtonpoloniosilva@gmail.com>
2018-10-07 00:14:02 -0300

    Projeto shape de interface

---
+++ src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+
+
+import entites.AbstractShape;
+import entites.Circle;
+import entites.Rectangle;
+import enums.Color;
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		AbstractShape s1 = new Circle(Color.BLACK, 2.0);
+		AbstractShape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
+		
+		System.out.println("Circle color: " + s1.getColor());
+		System.out.println("Circle area: " + String.format("%.3f", s1.area()));
+		System.out.println("Rectangle color: " + s2.getColor());
+		System.out.println("Rectangle area: " + String.format("%.3f", s2.area()));
+
+	}
+
+}
---
+++ src/entites/Shape.java
@@ -0,0 +1,6 @@
+package entites;
+
+public interface Shape {
+	double area();
+
+}
